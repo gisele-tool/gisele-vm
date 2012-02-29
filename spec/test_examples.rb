fb0b03f44330a93259505c3570370eafc237aca3
Bernard Lambeau <blambeau@gmail.com>
2012-02-29 10:38:51 +0100

    Check that the examples are correct (according to the Gvm grammar)

---
+++ spec/test_examples.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+module Gisele
+  class VM
+
+    shared_examples_for "A valid example file" do
+
+      it 'respect the .gvm grammar' do
+        sexpr = Gvm.sexpr(subject)
+        unless (Gvm === sexpr)
+          sexpr.sexpr_body.each do |block|
+            next if Gvm[:block]===block
+            block.sexpr_body[1..-1].each do |i|
+              raise "Bad instruction: #{i}" unless Gvm[:instruction]===i
+            end
+          end
+        end
+      end
+
+    end # A valid example file
+
+    examples = Path.backfind('.[examples]')/:examples
+    examples.glob("**/*.gvm").each do |file|
+      describe "the example #{file.basename}" do
+        subject{file}
+        it_behaves_like "A valid example file"
+      end
+    end
+
+  end
+end
