3b391e6912e6b1d8b83bdf796b9a63b5ce66c9a7
Bernard Lambeau <blambeau@gmail.com>
2012-03-02 16:04:30 +0100

    Add support for Hash arguments.

---
+++ spec/vm/gvm/grammar/test_hash.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+module Gisele
+  class VM
+    describe Gvm, "hash" do
+
+      def parse(src)
+        Gvm.parse(src, :root => :hash)
+      end
+
+      it 'parses empty hashes correctly' do
+        parse("{}").value.should eq({})
+      end
+
+      it 'parses singletons correctly' do
+        parse("{key: 12}").value.should eq({:key => 12})
+      end
+
+      it 'parses hashes correctly' do
+        parse("{key: 12, name: 'blaj'}").value.should eq({:key => 12, :name => 'blaj'})
+      end
+
+      it 'parses event dict' do
+        expected = {:hello => :s2, :goodbye => :s3}
+        parse("{hello: :s2, goodbye: :s3}").value.should eq(expected)
+      end
+
+    end
+  end
+end
