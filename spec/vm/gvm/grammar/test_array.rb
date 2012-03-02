1b9b0135f94632dbf219d6228bf10034a01abe2e
Bernard Lambeau <blambeau@gmail.com>
2012-03-02 15:40:38 +0100

    Add support for array arguments.

---
+++ spec/vm/gvm/grammar/test_array.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+module Gisele
+  class VM
+    describe Gvm, "array" do
+
+      def parse(src)
+        Gvm.parse(src, :root => :array)
+      end
+
+      it 'parses empty arrays correctly' do
+        parse("[ ]").value.should eq([])
+      end
+
+      it 'parses singletons correcttly' do
+        parse("[0]").value.should eq([0])
+      end
+
+      it 'parses arrays correcttly' do
+        parse("[0, 'blah', :symb]").value.should eq([0, 'blah', :symb])
+      end
+
+    end
+  end
+end
