61a36f37425e9e8964d47e45c53809b491eedd63
Bernard Lambeau <blambeau@gmail.com>
2012-03-04 12:19:56 +0100

    Strip .gvm files with 'ruby scripts/spaces'

--- scripts/spaces.rb
+++ scripts/spaces.rb
@@ -11,4 +11,5 @@ Dir["**/*.rb"].each{|file| clean_file(file)}
 Dir["**/*.gis"].each{|file| clean_file(file)}
 Dir["**/*.yml"].each{|file| clean_file(file)}
 Dir["**/*.md"].each{|file| clean_file(file)}
+Dir["**/*.gvm"].each{|file| clean_file(file)}
 Dir["**/*.gts"].each{|file| clean_file(file)}
