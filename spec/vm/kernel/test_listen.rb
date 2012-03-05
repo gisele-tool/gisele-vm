ce4e4140bf61d922e827f73525cdfaa8fec40e0b
Bernard Lambeau <blambeau@gmail.com>
2012-03-05 11:58:34 +0100

    Clean kernel's +listen+

--- spec/vm/kernel/test_listen.rb
+++ spec/vm/kernel/test_listen.rb
@@ -3,30 +3,34 @@ module Gisele
   class VM
     describe "kernel::listen" do
 
-      let(:vm)   { VM.new 0, Bytecode.kernel          }
-      let(:wlist){ {:ping => :sPing, :pong => :sPong} }
+      let(:list)  { ProgList.memory                       }
+      let(:vm)    { VM.new @parent, Bytecode.kernel, list }
+      let(:parent){ list.fetch(@parent)                   }
+      let(:wlist) { {:ping => :sPing, :pong => :sPong}    }
 
       before do
-        prog  = Prog.new(:pc => :listen)
-        @puid = vm.proglist.save(prog)
+        @parent = list.save Prog.new(:pc => :listen)
+        subject
       end
 
-      subject{
+      subject do
         vm.run(:listen, [ wlist ])
+      end
+
+      after do
         vm.stack.should be_empty
-        vm.proglist.fetch(@puid)
-      }
+      end
 
       it 'sets the events as waitlist' do
-        subject.waitlist.should eq(wlist)
+        parent.waitlist.should eq(wlist)
       end
 
       it 'sets the program counter to :react' do
-        subject.pc.should eq(:react)
+        parent.pc.should eq(:react)
       end
 
       it 'unschedules the current Prog' do
-        subject.progress.should be_false
+        parent.progress.should be_false
       end
 
     end
