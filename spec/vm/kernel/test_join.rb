406dfb5d573f3bb33faebd8484e6663464e52305
Bernard Lambeau <blambeau@gmail.com>
2012-03-05 11:49:26 +0100

    Clean kernel's +join+

--- spec/vm/kernel/test_join.rb
+++ spec/vm/kernel/test_join.rb
@@ -3,34 +3,40 @@ module Gisele
   class VM
     describe "kernel::join" do
 
-      let(:vm) { VM.new 0, Bytecode.kernel }
+      let(:list)  { ProgList.memory                       }
+      let(:vm)    { VM.new @parent, Bytecode.kernel, list }
+      let(:parent){ list.fetch(@parent)                   }
 
       before do
-        prog  = Prog.new(:pc => :join, :waitlist => wlist)
-        @puid = vm.proglist.save(prog)
+        @parent = list.save Prog.new(:pc => :join, :waitlist => wlist)
+        subject
       end
 
-      subject{
+      subject do
         vm.run(:join, [ {:wake => :wakeat} ])
-        vm.proglist.fetch(@puid)
-      }
+      end
 
       context 'when the waitlist is not empty' do
         let(:wlist){ {12 => true, 13 => true} }
 
         it 'keeps the program sleeping' do
-          subject.pc.should eq(:join)
-          subject.progress.should be_false
+          parent.pc.should eq(:join)
+          parent.progress.should be_false
+          pending{ vm.stack.should be_empty }
         end
       end
 
       context 'when the waitlist is empty' do
         let(:wlist){ {} }
 
+        after do
+          vm.stack.should be_empty
+        end
+
         it 'schedules the program at :wakeat' do
-          subject.pc.should eq(:wakeat)
-          subject.waitlist.should eq({})
-          subject.progress.should be_true
+          parent.pc.should eq(:wakeat)
+          parent.waitlist.should eq({})
+          parent.progress.should be_true
         end
       end
 
