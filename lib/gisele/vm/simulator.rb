f0c13ec04a09a264a2808165d2fcd720e3ea25a8
Bernard Lambeau <blambeau@gmail.com>
2012-03-09 12:52:20 +0100

    Revisit the Simulator implementation.

--- lib/gisele/vm/simulator.rb
+++ lib/gisele/vm/simulator.rb
@@ -5,18 +5,25 @@ module Gisele
     private
 
       def runone
-        if prog = vm.proglist.pick(:world)
-          vm.resume(prog, [ prog.waitlist.keys.sample ])
-          true
-        else
-          # let the superclass known that something is wrong so that
-          # it can manage the locks friendly (see Agent#run)
-          false
+        prog = nil
+
+        # Critical section to ensure that the agent won't be disconnected in
+        # the middle of an enactement step.
+        synchronize do
+          if prog = vm.proglist.pick(:world)
+            event = prog.waitlist.keys.sample
+            debug("Sending event #{event} to Prog #{prog.puid}@#{prog.pc}")
+            vm.resume(prog, [ event ])
+          end
         end
+
+        # No prog probably means that the VM is currently trying to disconnect.
+        # We wait 0.1 msec out of the critical section to favor the disconnection
+        # process.
+        sleep(0.1) unless prog
+
       rescue Exception => ex
-        puid = (prog && prog.puid) || ''
-        msg  = "Simulation error (#{puid}): #{ex.message}\n" + ex.backtrace.join("\n")
-        error(msg) rescue nil
+        error error_message(ex, "Simulation error:")
       end
 
     end # class Simulator
