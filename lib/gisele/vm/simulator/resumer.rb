c2e2fa0a95f6f5df6d626fd04f28364e3aba0497
Bernard Lambeau <blambeau@gmail.com>
2012-03-09 13:08:06 +0100

    Simulator -> Simulator::Resumer.

--- lib/gisele/vm/simulator/resumer.rb
+++ lib/gisele/vm/simulator/resumer.rb
@@ -1,31 +1,42 @@
 module Gisele
   class VM
-    class Simulator < Agent
+    class Simulator
+      class Resumer < Agent
 
-    private
-
-      def runone
-        prog = nil
-
-        # Critical section to ensure that the agent won't be disconnected in
-        # the middle of an enactement step.
-        synchronize do
-          if prog = vm.proglist.pick(:world)
-            event = prog.waitlist.keys.sample
-            debug("Sending event #{event} to Prog #{prog.puid}@#{prog.pc}")
-            vm.resume(prog, [ event ])
-          end
+        def default_options
+          { :sleep_time => nil }
         end
 
-        # No prog probably means that the VM is currently trying to disconnect.
-        # We wait 0.1 msec out of the critical section to favor the disconnection
-        # process.
-        sleep(0.1) unless prog
+        private
 
-      rescue Exception => ex
-        error error_message(ex, "Simulation error:")
-      end
+          def runone
+            prog = nil
 
+            # Critical section to ensure that the agent won't be disconnected in
+            # the middle of an enactement step.
+            synchronize do
+              if prog = vm.proglist.pick(:world)
+                event = prog.waitlist.keys.sample
+                debug("Sending event #{event} to Prog #{prog.puid}@#{prog.pc}")
+                vm.resume(prog, [ event ])
+              end
+            end
+
+            if prog
+              sleep(options[:sleep_time] || 0)
+            else
+              # No prog probably means that the VM is currently trying to disconnect.
+              # We wait 0.1 msec out of the critical section to favor the disconnection
+              # process.
+              sleep(0.1)
+            end
+
+          rescue Interrupt
+          rescue Exception => ex
+            error error_message(ex, "Simulation error:")
+          end
+
+      end # class Resumer
     end # class Simulator
   end # class VM
 end # module Gisele
