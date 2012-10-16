f84798db1f9576e7f2d19e2e9032e387eedc6c78
Bernard Lambeau <blambeau@gmail.com>
2012-10-16 15:59:19 +0200

    Fix drb listener.

--- examples/listening/drb.rb
+++ examples/listening/drb.rb
@@ -2,12 +2,17 @@ require 'gisele-vm'
 
 class Listener
   include DRb::DRbUndumped
+
+  def initialize(vm)
+    @vm = vm
+  end
+  attr_reader :vm
   
   def call(event)
     prog      = event.prog
     type      = event.type
     task_name = event.args.first
-    puts "#{task_name}:#{type} (#{prog.root})"
+    puts "#{task_name}:#{type} (#{prog.puid}, #{prog.root})"
   end
   
 end
@@ -15,11 +20,6 @@ end
 Gisele::VM.drb_client do |vm|
 
   # Subscribe our listener
-  vm.subscribe Listener.new
+  vm.subscribe Listener.new(vm)
 
-  # trap CTRL-C and shut down the VM
-  trap('INT'){
-    vm.info "Interrupt on user request (graceful shutdown)."
-    vm.stop
-  }
 end
